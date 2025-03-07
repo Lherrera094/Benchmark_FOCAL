ea46a39f5154779958b4334a54dfcab080b53ac4
Lherrera094 <herrera.woa94@gmail.com>
2025-03-07 04:46:45 -0600

    Update FOCAL standard code for Benchmark

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# Benchmark_FOCAL
-Repository shows the benchmarking of the code FOCAL with the theory of plasma physics.
+<h3 align="center"> FOCAL </h3>
+
+Developers: [Alf Köhn-Seemann](https://www.igvp.uni-stuttgart.de/team/Koehn-Seemann/)\
+&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;&nbsp;[Luis Carlos Herrera Quesada](https://www.linkedin.com/in/lherreraquesada/)
+	    
+**Desription**: 3D FDTD code for propagation of electromagnetic waves in cold magnetized plasma.
+
+* **Features**: FOCAl receives a JSON as an input file, using the cJSON library provided by Dave Gamble, see https://github.com/DaveGamble/cJSON, allowing a faster and easiest operation. Focal have a series of plasma density profiles that can be selected by the user, furthermore, the code is capable of reading external profiles developed by the user. FOCAL is fully parallelized with OpenMP. User may choose between 3 different implemented boundaries: ABC, MUR and UPML. The output consists of a text file for the energy power values, a JSON copyfile of the input values and a HDF5 file containing the absolute electric field, plasma density volume, antenna fields and positions, and system's characteristics. Code has been benchmarked for vacuum propagation, power calculation, against EMIT-3D and cold plasma theory (to be updated).
+
+* **Execute**:             1. Clone repository.\
+&emsp;&emsp;&emsp;&emsp;   2. Open terminal in FOCAL folder. Folders "include", "src" and "tools" should be visible, along with "input_FOCAL.json", "LICENSE", "Makefile" and "README.md" files.\
+&emsp;&emsp;&emsp;&emsp;   3. Write on terminal: make. If compilation is succesfull, bin and build folder should been created without errors.\
+&emsp;&emsp;&emsp;&emsp;   4. On "inpute_FOCAL.json" write desired parameters.\
+&emsp;&emsp;&emsp;&emsp;   5. To execute FOCAL, write on terminal: bin/./exe 
+
+* **Simulation Example**: 
+
+<p align="center">
+  
+![UPML_sim](/tools/simulation_PML10_testLog.gif "UPML simulation.")
+  
+</p>
+
+* **Tools**: Contains a series of Python codes to help user in the input profiles, and results visualization. 
+
+* **Future additions**: Helical antenna for modes m=0 and m=1 plasma injection.
+
+Note that this code is still evolving. 
---
+++ src/background_profiles.c
@@ -0,0 +1,239 @@
+#include "background_profiles.h"
+
+void init_background_profiles(  gridConfiguration *gridCfg, 
+                                beamAntennaConfiguration *beamCfg,
+                                saveData *saveDCfg,
+                                double n_e[NX/2][NY/2][NZ/2], 
+                                double J_B0[NX][NY][NZ] ){
+    //{{{
+
+    printf( "Starting defining background plasma density...\n" );
+            // ne_profile: 1 = plasma mirror
+            //             2 = linearly increasing profile
+    make_density_profile( gridCfg, saveDCfg, 
+            // cntrl_para: ne_profile=1 --> 0: plane mirror; oblique mirror: -.36397; 20 degrees: -.17633
+            //             ne_profile=2 --> k0*Ln: 25
+            k0Ln_at_X1,
+            n_e );
+    // set plasma density in absorber smoothly to zero
+    //set_densityInAbsorber_v2( gridCfg, "z1", n_e );             // defined in focal.c
+    set_densityInAbsorber_v2( gridCfg, "x1x2y1y2z1", n_e );
+    printf( "...done defining background plasma density\n" );
+
+    printf( "starting defining background magnetic field...\n" );
+    // B0x: even-odd-odd
+    // B0y: odd-even-odd
+    // B0z: odd-odd-even
+            // B0_profile: 1 = constant field
+    make_B0_profile(
+            gridCfg, saveDCfg, 
+            // cntrl_para: B0_profile=1 --> value of Y
+            Y_at_X1, 
+            J_B0 );
+    printf( "...done defining background magnetic field\n" );
+
+}//}}}
+ 
+
+int make_density_profile( gridConfiguration *gridCfg, 
+                          saveData *saveDCfg,
+                          double cntrl_para, 
+                          double n_e[NX/2][NY/2][NZ/2] ) {
+//{{{
+    // This function allows to defines the plasma density profiles. The
+    // parameter "cntrl_para" allows to switch between varies options. The
+    // plasma density is written into the array "n_e", which has half of the 
+    // resoltion of the full-wave grid (half of the size into each direction),
+    // as 1 Yee-cell consists of 2 grid points and we don't want the background
+    // to vary inside of 1 Yee-cell.
+    // Option 5 might be the most usefull option, as it simply reads the data
+    // from an hdf5 file (for further details, see below in the code).
+
+    size_t
+        ii, jj, kk, 
+        ne_start_z;
+    double
+        ne_max,
+        ne_k0Ln,
+        aux;
+    char filename_input_grid[PATH_MAX];
+
+    // if density is larger than this value, FDTD code becomes instable
+    ne_max  = PERIOD * 2./5.;
+
+    sprintf( filename_input_grid, "%s", file_input);
+
+    if ( ne_profile == 1 ) {
+        // plasma mirror
+        for (ii=0 ; ii<(NX/2) ; ++ii) {
+            for (jj=0 ; jj<(NY/2) ; ++jj) {
+                //for (kk=((gridCfg->NZ-gridCfg->d_absorb-4)/2) ; kk<(gridCfg->NZ/2) ; ++kk) {
+                for (kk=0 ; kk<(NZ/2) ; ++kk) {
+                    // z = m*y + b = delta_z/delta_y * y + z_start
+                    //             = cntrl_para * y + z_start
+                    if ( (double)kk > (cntrl_para*(double)jj + ((double)NZ-(double)D_ABSORB-4.)/2.) ) {
+                        n_e[ii][jj][kk] = ne_max;
+                    }
+                }
+            }
+        }
+    } else if ( ne_profile == 2 ) {
+        // linearly increasing profile with k0Ln as slope
+        // n_e(z) = m*z 
+        // with m = 2*pi/(k0Ln*lambda) 
+        //      z = z-starting_position
+        ne_start_z  = (D_ABSORB + PERIOD)/2;
+        //ne_start_z  = (gridCfg->d_absorb + 224.155)/2;    // benchmark scenario from STEP project: .15m/l_0*period
+        if (ne_start_z%2 != 0)
+            ne_start_z  += 1;
+        ne_k0Ln     = cntrl_para;
+        printf( "make_density_profile: ne_profile = %d, ne_start_z = %ld, k0Ln = %f\n", 
+                ne_profile, ne_start_z, ne_k0Ln );
+        for (ii=0 ; ii<(NX/2) ; ++ii) {
+            for (jj=0 ; jj<(NY/2) ; ++jj) {
+                for (kk=0 ; kk<(NZ/2) ; ++kk) {
+                    aux = ((double)kk - (double)ne_start_z) * (2.*M_PI / (ne_k0Ln*PERIOD));
+                    // negative density values are unphysical
+                    if (aux < .0)
+                        aux = .0;
+                    // FDTD full-wave codes only allow for a maximum density value
+                    if (aux > ne_max) {
+                        aux  = ne_max;
+                        printf( "    maximum density achieved (ii, jj, kk = %ld, %ld, %ld): %f\n", ii, jj, kk, aux );
+                    }
+                    //if ((ii%10 == 0) && (jj%10 == 0) && (kk%10 == 0))
+                    //if (kk%10 == 0)
+                    //    printf( "    n_e[%4ld][%4ld][%4ld] = %f\n", ii, jj, kk, aux );
+                    n_e[ii][jj][kk] = aux;
+                }
+            }
+        }
+    } else if ( ne_profile == 3 ) {
+        // plasma cylinder (2D gauss) in center of grid
+        //
+        //                               (y-y0)^2        (z-z0)^2
+        // n_e(y,z) = n_e,max * exp( -( ------------ + ------------- ) )
+        //                               2*sigma_y^2    2*sigma_z^2
+        //
+        for (ii=0 ; ii<(NX/2) ; ++ii) {
+            for (jj=0 ; jj<(NY/2) ; ++jj) {
+                for (kk=0 ; kk<(NZ/2) ; ++kk) {
+                    n_e[ii][jj][kk]    = exp( -1.* (
+                                                 pow((double)jj-(double)NY/4., 2)/(2*pow(PERIOD/2.,2)) 
+                                                +pow((double)kk-(double)NZ/4., 2)/(2*pow(PERIOD/2.,2))
+                                             )) * 5.;
+                }
+            }
+        }
+    } else if ( ne_profile == 4 ) {
+        // same as ne_profile = 3, but plasma cylinder is now along y
+        for (ii=0 ; ii<(NX/2) ; ++ii) {
+            for (jj=0 ; jj<(NY/2) ; ++jj) {
+                for (kk=0 ; kk<(NZ/2) ; ++kk) {
+                    n_e[ii][jj][kk]    = exp( -1.* (
+                                                 pow((double)ii-(double)NX/4., 2)/(2*pow(PERIOD/2.,2)) 
+                                                +pow((double)kk-(double)NZ/4., 2)/(2*pow(PERIOD/2.,2))
+                                             )) * 2;//5.;
+                }
+            }
+        }
+    } else if ( ne_profile == 5 ) {
+        // filename and dataset-name are currently hard-coded
+        //   ==> change this
+        //       either provide additional parameter in function call
+        //       or not load the profile here, but directly in main
+        //readMyHDF( NX/2, NY/2, NZ/2, filename_input_grid, "n_e", n_e );
+
+        // ne_tmp allows to modify the original ne-profile load from hdf5-file
+        // mostly used for debugging purposes
+        double (*ne_tmp)[NY/2][NZ/2]  = calloc(NX/2, sizeof *ne_tmp);
+        readMyHDF( NX/2, NY/2, NZ/2, filename_input_grid, "n_e", ne_tmp );
+        for (ii=0 ; ii<NX ; ii+=2) {
+            for (jj=0 ; jj<NY ; jj+=2) {
+                for (kk=0 ; kk<NZ ; kk+=2) {
+                    //n_e[(int)(ii/2)][(int)(jj/2)][(int)(kk/2)] = ne_tmp[(NX/2 - ii/2)][jj/2][kk/2;    // flip-x
+                    //n_e[(int)(ii/2)][(int)(jj/2)][(int)(kk/2)] = ne_tmp[ii/2][(NY/2 - jj/2)][kk/2];   // flix-y
+                    n_e[(int)(ii/2)][(int)(jj/2)][(int)(kk/2)] = ne_tmp[ii/2][jj/2][kk/2];
+                }
+            }
+        }
+    }
+    return EXIT_SUCCESS;
+}//}}}
+
+
+int make_B0_profile( gridConfiguration *gridCfg, 
+                     saveData *saveDCfg,
+                     double cntrl_para, 
+                     double J_B0[NX][NY][NZ] ) {
+//{{{
+    size_t
+        ii, jj, kk; 
+    char filename_input_grid[PATH_MAX];
+//    double
+//        aux;
+
+    sprintf( filename_input_grid, "%s", file_input);
+
+    // B0x: even-odd-odd
+    // B0y: odd-even-odd
+    // B0z: odd-odd-even
+    if ( B0_profile == 1 ) {
+        // constant field
+        for (ii=0 ; ii<NX ; ii+=2) {
+            for (jj=0 ; jj<NY ; jj+=2) {
+                for (kk=0 ; kk<NZ ; kk+=2) {
+                    J_B0[ii  ][jj+1][kk+1] = cntrl_para;
+                    J_B0[ii+1][jj  ][kk+1] = cntrl_para*.0;
+                    J_B0[ii+1][jj+1][kk  ] = cntrl_para*.0;
+                }
+            }
+        }
+    } else if ( B0_profile == 2 ) {
+        // filename and dataset-name are currently hard-coded
+        // (just an urgent, quick, and dirty hack, sorry)
+        //   ==> change this
+        //       either provide additional parameter in function call
+        //       or not load the profile here, but directly in main
+        
+        double (*B0_tmp)[NY/2][NZ/2]  = calloc(NX/2, sizeof *B0_tmp);
+
+        // B0_x
+        //readMyHDF( NX/2, NY/2, NZ/2, "input/grid.h5", "B0_x", B0_tmp );
+        readMyHDF( NX/2, NY/2, NZ/2, filename_input_grid, "B0_x", B0_tmp );
+        for (ii=0 ; ii<NX ; ii+=2) {
+            for (jj=0 ; jj<NY ; jj+=2) {
+                for (kk=0 ; kk<NZ ; kk+=2) {
+                    J_B0[ii  ][jj+1][kk+1] = B0_tmp[ii/2][jj/2][kk/2];
+                    B0_tmp[ii/2][jj/2][kk/2] = .0;
+                }
+            }
+        }
+        // B0_y
+        //readMyHDF( NX/2, NY/2, NZ/2, "input/grid.h5", "B0_y", B0_tmp );
+        readMyHDF( NX/2, NY/2, NZ/2, filename_input_grid, "B0_y", B0_tmp );
+        for (ii=0 ; ii<NX ; ii+=2) {
+            for (jj=0 ; jj<NY ; jj+=2) {
+                for (kk=0 ; kk<NZ ; kk+=2) {
+                    J_B0[ii+1][jj  ][kk+1] = B0_tmp[ii/2][jj/2][kk/2];
+                    B0_tmp[ii/2][jj/2][kk/2] = .0;
+                }
+            }
+        }
+        // B0_z
+        //readMyHDF( NX/2, NY/2, NZ/2, "input/grid.h5", "B0_z", B0_tmp );
+        readMyHDF( NX/2, NY/2, NZ/2, filename_input_grid, "B0_z", B0_tmp );
+        for (ii=0 ; ii<NX ; ii+=2) {
+            for (jj=0 ; jj<NY ; jj+=2) {
+                for (kk=0 ; kk<NZ ; kk+=2) {
+                    J_B0[ii+1][jj+1][kk  ] = B0_tmp[ii/2][jj/2][kk/2];
+                    B0_tmp[ii/2][jj/2][kk/2] = .0;
+                }
+            }
+        }
+
+        free( B0_tmp );
+    }
+
+    return EXIT_SUCCESS;
+}//}}}
